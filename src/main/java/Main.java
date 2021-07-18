a3d218eea66b676a5cd74cbd2a5210c14209f251
JamDaBam <->
2021-07-18 17:37:03 +0200

    Pdf-Recognition

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,5 @@
-import org.ghost4j.document.DocumentException;
-import org.ghost4j.document.PDFDocument;
-import org.ghost4j.renderer.RendererException;
-import org.ghost4j.renderer.SimpleRenderer;
-
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.List;
-
 public class Main {
-    public static void main(String[] args) throws IOException, RendererException, DocumentException {
+    public static void main(String[] args) {
         TextRecognition textRecognition = TextRecognition.getInstance();
 
 //        InputStream resourceAsStream = Main.class.getClassLoader().getResourceAsStream("pic.png");
@@ -19,28 +8,10 @@ public class Main {
 //
 //        System.out.print("Hier muss was stehen:\n" + text);
 
-        PDFDocument pdf = new PDFDocument();
-        pdf.load(Main.class.getClassLoader().getResourceAsStream("test.pdf"));
-
-        SimpleRenderer renderer = new SimpleRenderer();
-        renderer.setResolution(300);
-        List<Image> images = renderer.render(pdf);
-
-        int site = 1;
-
-        for (Image image : images) {
-            BufferedImage bi = imageToBufferedImage(image);
-            String pdftext = textRecognition.read(bi);
-            bi.flush();
-            System.out.println("Seite " + site++ + ":\n" + pdftext + "\n");
+        String[] read = textRecognition.read("C:\\Users\\Emulator\\IdeaProjects\\TextScanner\\src\\main\\resources\\test.pdf");
+        for (int i = 0; i < read.length; i++) {
+            String s = read[i];
+            System.out.println("Seite " + (i + 1) + "\n" + s);
         }
-    }
-
-    public static BufferedImage imageToBufferedImage(Image aImage) {
-        BufferedImage bi = new BufferedImage(aImage.getWidth(null), aImage.getHeight(null), BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g2d = bi.createGraphics();
-        g2d.drawImage(aImage, 0, 0, null);
-        g2d.dispose();
-        return bi;
     }
 }
--- src/main/java/TextRecognition.java
+++ src/main/java/TextRecognition.java
@@ -2,12 +2,18 @@ import net.sourceforge.tess4j.ITessAPI;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
 import net.sourceforge.tess4j.util.LoadLibs;
+import org.ghost4j.document.DocumentException;
+import org.ghost4j.document.PDFDocument;
+import org.ghost4j.renderer.RendererException;
+import org.ghost4j.renderer.SimpleRenderer;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 public class TextRecognition {
     private static TextRecognition cvInstance;
@@ -40,6 +46,38 @@ public class TextRecognition {
         return tesseract;
     }
 
+    public String[] read(String aPdfFilePath) {
+        String[] result;
+
+        try {
+            PDFDocument pdf = new PDFDocument();
+            pdf.load(new File(aPdfFilePath));
+
+            SimpleRenderer renderer = new SimpleRenderer();
+            renderer.setResolution(300);
+            List<Image> images = renderer.render(pdf);
+
+            result = new String[images.size()];
+
+            int site = 1;
+
+            for (int i = 0; i < images.size(); i++) {
+                Image image = images.get(i);
+                BufferedImage bi = imageToBufferedImage(image);
+                String pdftext = read(bi);
+                bi.flush();
+                result[i] = pdftext;
+                System.out.println("Seite " + site++ + ":\n" + pdftext + "\n");
+            }
+
+        } catch (IOException | DocumentException | RendererException e) {
+            e.printStackTrace();
+            result = new String[]{"Es ist ein Fehler aufgetreten:\n" + e.getMessage()};
+        }
+
+        return result;
+    }
+
     public String read(InputStream aInputStream) {
         String result;
 
@@ -65,5 +103,13 @@ public class TextRecognition {
         }
 
         return result;
+    }
+
+    private BufferedImage imageToBufferedImage(Image aImage) {
+        BufferedImage bi = new BufferedImage(aImage.getWidth(null), aImage.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = bi.createGraphics();
+        g2d.drawImage(aImage, 0, 0, null);
+        g2d.dispose();
+        return bi;
     }
 }
